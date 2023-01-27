00850bdef764166f26e47bcbdb82b96faa5caa74
AntonZinchenko32 <zinchenko309@gmail.com>
2023-01-27 08:41:31 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+});
